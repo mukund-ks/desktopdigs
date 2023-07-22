ba617253e5c2faa6456bb897f6a25ff4c8602f1c
Mukund Kumar <banananarick69@gmail.com>
2023-07-23 02:21:22 +0530

    animation & spacers

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from 'prop-types';
 import { IconButton, Typography, Spinner } from "@material-tailwind/react";
+import { motion, easeInOut } from "framer-motion";
 import axios from '@/api/axios.js';
+import './GalleryStyles.css';
 
 Images.propTypes = {
     imageURL: PropTypes.string,
     id: PropTypes.number,
+    currentPage: PropTypes.number,
 };
 
 function Images(props) {
     return (
         <React.Fragment key={props.id}>
-            <div className="bg-transparent overflow-hidden rounded-lg shadow-md w-full h-full object-cover">
-                <img src={props.imageURL} id={props.id} alt={`image-${props.id}`} />
-            </div>
+            <motion.div
+                className="bg-transparent overflow-hidden rounded-lg shadow-md"
+                key={props.imageURL}
+                initial={{ opacity: 0, x: -10 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ ease: easeInOut, duration: 0.7 }}
+            >
+                <img
+                    src={props.imageURL}
+                    id={props.id}
+                    alt={`image-${props.id}`}
+                    width={700}
+                    height={500}
+                    loading="lazy" />
+            </motion.div>
         </React.Fragment>
     );
 }
@@ -64,21 +79,55 @@ export default function Gallery() {
         setCurrentPage(currentPage - 1);
     };
 
-    console.log(currentPage);
-
     return (
-        <div>
-            <section className="h-[100vh] w-auto items-center flex flex-row justify-center snap-center">
-                <Typography variant='h1' className='text-myRed3'>The Gallery</Typography>
+        <div className="flex flex-col absolute">
+            <section className="h-[100vh] w-auto items-center flex flex-col justify-center snap-center gap-1" id="1">
+                <motion.div
+                    initial={{ opacity: 0, y: '10px' }}
+                    whileInView={{ opacity: 1, y: '0px' }}
+                    transition={{ ease: easeInOut, duration: 0.8 }}
+                >
+                    <Typography variant='h1' className='text-myRed3'>The Gallery</Typography>
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0, x: '10px' }}
+                    whileInView={{ opacity: 1, x: '0px' }}
+                    transition={{ ease: easeInOut, duration: 0.8 }}
+                >
+                    <Typography variant='lead' className='text-myGray'>all the {imgsCount} images.</Typography>
+                </motion.div>
+                <motion.div
+                    className='fixed bottom-[125px] left-0 right-0 z-20 flex justify-center'
+                    animate={{
+                        y: [1, 10, 1]
+                    }}
+                    transition={{ ease: easeInOut, repeat: Infinity, repeatDelay: 5, duration: 0.5 }}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 fill-mywhite">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                    </svg>
+                </motion.div>
             </section>
-            <section className="mb-20 flex flex-col items-center snap-center">
+            <motion.div
+                className='spacer layer4 relative md:bottom-[120px] bottom-[90px]'
+                initial={{ opacity: 0, y: -10 }}
+                whileInView={{ opacity: 1, y: 5 }}
+                whileHover={{ scale: 1.03 }}
+                transition={{ ease: easeInOut, duration: 0.9 }}
+            ></motion.div>
+            <section className="mb-20 flex flex-col items-center snap-center w-auto h-auto">
                 {
                     (images.length > 0) ? (
                         <React.Fragment>
                             <div className="grid grid-cols-3 gap-3 mx-[20px] mb-[20px]">
                                 {
                                     currentImgs.map((img, id) => (
-                                        <Images imageURL={img.imageURL} id={id} key={id} />
+                                        <Images
+                                            imageURL={img.imageURL}
+                                            id={id}
+                                            key={id}
+                                            currentPage={currentPage}
+                                        />
                                     ))
                                 }
                             </div>
@@ -120,6 +169,13 @@ export default function Gallery() {
                     </IconButton>
                 </div>
             </section>
+            <motion.div
+                className='spacer layer0 relative md:bottom-[70px] bottom-[90px]'
+                initial={{ opacity: 0, y: 10 }}
+                whileInView={{ opacity: 1, y: -5 }}
+                whileHover={{ scale: 1.03 }}
+                transition={{ ease: easeInOut, duration: 0.7 }}
+            ></motion.div>
         </div>
     );
 }
