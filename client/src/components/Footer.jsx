8119bca41e6e17114c52a189893d638fa76b1196
Mukund Kumar <sankhyan.mukund@gmail.com>
2023-08-05 16:13:50 +0530

    Minor Footer Change

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,14 +6,16 @@ const Footer = () => {
     return (
         <React.Fragment>
             <footer className="fixed bottom-0 z-50 flex w-full flex-row flex-wrap justify-center gap-y-6 gap-x-12 py-6 px-6 mt-6 text-center md:justify-between">
-                <Typography color="blue-gray" className="font-normal">
-                    <span className="flex flex-row text-mywhite">
-                        Made with
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-2 fill-mywhite">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                        </svg>
-                    </span>
-                </Typography>
+                <div>
+                    <Typography color="white" className="font-normal">
+                        <span className="flex flex-row text-mywhite">
+                            Made with
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-2 fill-mywhite">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                            </svg>
+                        </span>
+                    </Typography>
+                </div>
                 <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
                     <li>
                         <Typography
@@ -48,7 +50,7 @@ const Footer = () => {
                     <li>
                         <Typography
                             as="a"
-                            href="mailto:sankhyan.mukund@gmail.com"
+                            href="mailto:mukund.28.k@gmail.com"
                             target='_blank'
                             color="blue-gray"
                             className="font-normal transition-colors text-mywhite hover:text-myGray"
