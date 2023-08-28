c78af5221b379381d891edd9f12555d77a6e862c
Mukund Kumar <sankhyan.mukund@gmail.com>
2023-08-28 22:31:01 +0530

    responsive about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -22,11 +22,11 @@ export default function About() {
         show: { opacity: 1, x: 0 },
     }
 
-    const para = "Welcome to DesktopDigs! I'm Mukund Kumar, the creator and driving force behind this project.\n At its heart, DesktopDigs is a space where my two passions, coding and ingame photography converge to bring about a platform for showcasing the clicks, carefully curated and presented to highlight the beauty that often rushes by in the heat of the game. Thank you for taking the time to explore my project. Your interest means a lot.\n If you come across any aspects of the project that you think could be made better, I invite you to consider contributing to the project's GitHub repository. Your involvement would be greatly valued."
+    const para = "Welcome to DesktopDigs! I'm Mukund Kumar, the creator and driving force behind this project.\n At its heart, DesktopDigs is a space where my two passions, coding and ingame photography converge to bring about a platform for showcasing the clicks, carefully curated and presented to highlight the beauty that often rushes by in the heat of the game.\n Thank you for taking the time to explore my project. Your interest means a lot.\n If you come across any aspects of the project that you think could be made better, I invite you to consider contributing to the project's GitHub repository. Your involvement would be greatly valued."
 
     return (
         <div className="flex flex-col absolute">
-            <section className="w-[100vw] h-[100vh] items-center flex flex-col justify-center gap-y-20 md:snap-center">
+            <section className="w-[100vw] h-[100vh] items-center flex flex-col justify-center gap-y-7 md:gap-y-20 md:snap-center">
                 <div className="text-center">
                     <motion.div
                         initial={{ opacity: 0, y: '-10px' }}
