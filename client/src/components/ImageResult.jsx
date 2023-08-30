60d1b425ce957177cede5b6fb04cd46172c66c64
Mukund Kumar <sankhyan.mukund@gmail.com>
2023-08-30 14:13:03 +0530

    Added image modal, closes #3

--- client/src/components/ImageResult.jsx
+++ client/src/components/ImageResult.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Viewer from 'react-viewer';
 import { motion, easeInOut } from "framer-motion";
 import { Typography } from "@material-tailwind/react";
 import PropTypes from 'prop-types';
@@ -11,10 +12,12 @@ Images.propTypes = {
 };
 
 export default function Images(props) {
+    const [visible, setVisible] = useState(false);
+
     return (
         <React.Fragment key={props.id}>
             <motion.div
-                className="bg-transparent overflow-hidden rounded-lg shadow-md"
+                className="bg-transparent overflow-hidden rounded-lg shadow-md cursor-pointer"
                 key={props.imageURL}
                 initial={{ opacity: 0, x: -10 }}
                 whileInView={{ opacity: 1, x: 0 }}
@@ -28,12 +31,33 @@ export default function Images(props) {
                     #{props.tags[0]}{' '}#{props.tags[1]}
                 </Typography>
                 <img
+                    onClick={() => setVisible(true)}
                     src={props.imageURL}
                     id={props.id}
                     alt={`image-${props.id}`}
                     width={700}
                     height={500}
-                    loading="lazy" />
+                    loading="lazy"
+                />
+                <Viewer
+                    visible={visible}
+                    onClose={() => { setVisible(false); }}
+                    images={
+                        [
+                            {
+                                src: props.imageURL,
+                                alt: `#${props.tags[0]} #${props.tags[1]}`,
+                                downloadUrl: props.imageURL
+                            }
+                        ]
+                    }
+                    downloadable
+                    downloadInNewWindow
+                    exportFileName={`image-${props.tags[0]}-${props.tags[1]}-DesktopDigs`}
+                    noNavbar
+                    showTotal={false}
+                    noImgDetails
+                />
             </motion.div>
         </React.Fragment>
     );
