12c0d8015961f4d636773f2940d970f0cca6777e
Mukund Kumar <sankhyan.mukund@gmail.com>
2023-09-02 01:58:06 +0530

    profile dialog done

--- client/src/components/ProfileDialog.jsx
+++ client/src/components/ProfileDialog.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from 'prop-types';
 import {
-    // Button,
+    Button,
     Dialog,
     DialogHeader,
     DialogBody,
     DialogFooter,
+    Typography,
 } from "@material-tailwind/react";
+import AuthContext from "../context/AuthProvider";
 
 ProfileDialog.propTypes = {
     profileDialog: PropTypes.bool,
@@ -14,20 +16,85 @@ ProfileDialog.propTypes = {
 };
 
 export default function ProfileDialog(props) {
+    const { auth } = useContext(AuthContext);
     return (
         <React.Fragment>
             <Dialog
                 open={Boolean(props.profileDialog)}
                 handler={props.handleProfileDialog}
-                size="md"
-                className="bg-transparent shadow-none"
+                size="sm"
+                className="bg-myBlack/40 shadow-none"
             >
-                <DialogHeader>Profile</DialogHeader>
-                <DialogBody divider>
-                    Profile Body
+                <DialogHeader>
+                    <Typography
+                        variant="h3"
+                        className="text-myRed3 tracking-wide"
+                    >
+                        Hi, {auth.username}
+                    </Typography>
+                </DialogHeader>
+                <DialogBody>
+                    <div className="flex flex-col gap-y-2">
+                        <Typography
+                            variant="h4"
+                            className="text-myRed3/75 tracking-wide"
+                        >
+                            Account Details
+                        </Typography>
+                        <div className="flex flex-col">
+                            <Typography
+                                variant="lead"
+                                className="text-myRed2 tracking-wide"
+                            >
+                                Name
+                            </Typography>
+                            <Typography
+                                variant="paragraph"
+                                className="text-myGray text-lg tracking-wide"
+                            >
+                                {auth.name}
+                            </Typography>
+                        </div>
+                        <div className="flex flex-col">
+                            <Typography
+                                variant="lead"
+                                className="text-myRed2 tracking-wide"
+                            >
+                                Email
+                            </Typography>
+                            <Typography
+                                variant="paragraph"
+                                className="text-myGray text-lg tracking-wide"
+                            >
+                                {auth.email}
+                            </Typography>
+                        </div>
+                        <div className="flex flex-col">
+                            <Typography
+                                variant="lead"
+                                className="text-myRed2 tracking-wide"
+                            >
+                                Admin?
+                            </Typography>
+                            <Typography
+                                variant="paragraph"
+                                className="text-myGray text-lg tracking-wide"
+                            >
+                                {auth.admin ? "Yes" : "No"}
+                            </Typography>
+                        </div>
+                    </div>
                 </DialogBody>
                 <DialogFooter>
-                    Profile Footer
+                    <Button
+                        variant="filled"
+                        size="sm"
+                        color="white"
+                        className="rounded-full bg-transparent shadow-none focus:shadow-none active:shadow-none hover:shadow-none border-2 border-myRed3/90 text-mywhite hover:border-myRed3/70 hover:text-mywhite/70"
+                        ripple={false}
+                    >
+                        Change Password
+                    </Button>
                 </DialogFooter>
             </Dialog>
         </React.Fragment>
