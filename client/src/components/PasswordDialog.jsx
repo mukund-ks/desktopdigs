e6c495392eb0bf10e63feb1d4add39c605a47993
Mukund Kumar <sankhyan.mukund@gmail.com>
2023-09-05 01:19:42 +0530

    handled post password change events & removed TODOs

--- client/src/components/PasswordDialog.jsx
+++ client/src/components/PasswordDialog.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import PropTypes from 'prop-types';
 import {
     Typography,
@@ -11,25 +11,28 @@ import {
     Alert,
 } from '@material-tailwind/react';
 import axios from '@/api/axios.js';
-
-//TODO:
-//  * Router Link for 'Change Password'?
-//  * Handle events after Password Change.
+import AuthContext from "../context/AuthProvider";
+import PostChange from "./PostChange";
 
 PasswordDialog.propTypes = {
     passwordDialog: PropTypes.bool,
     handlePasswordDialog: PropTypes.func,
+    handleProfileDialog: PropTypes.func,
 };
 
 const PASSWORD_URL = '/api/user/change-password';
 
 export default function PasswordDialog(props) {
-    const [email, setEmail] = useState("");
     const [oldPass, setOldPass] = useState("");
     const [newPass, setNewPass] = useState("");
     const [checkPass, setCheckPass] = useState(false);
     const [success, setSuccess] = useState(false);
     const [errMsg, setErrMsg] = useState("");
+    const { auth } = useContext(AuthContext);
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [oldPass, newPass]);
 
     const handlePassChange = async (e) => {
         e.preventDefault();
@@ -37,7 +40,7 @@ export default function PasswordDialog(props) {
             const res = await axios.post(PASSWORD_URL,
                 JSON.stringify(
                     {
-                        email: email,
+                        email: auth.email,
                         oldPass: oldPass,
                         newPass: newPass,
                     }
@@ -55,7 +58,7 @@ export default function PasswordDialog(props) {
             } else if (error.response?.status === 401) {
                 setErrMsg(error.response?.data?.message);
             } else if (error.response?.status === 404) {
-                setErrMsg(error.response?.data?.message);
+                setErrMsg("User not found");
             } else {
                 setErrMsg("Password Change Failed");
             }
@@ -70,7 +73,7 @@ export default function PasswordDialog(props) {
                 size="xs"
                 className="bg-transparent shadow-none"
             >
-                <Card className="mx-auto w-full max-w-[24rem] bg-myBlack/40 shadow-inner">
+                <Card className="mx-auto w-full max-w-[24rem] bg-myBlack/50 shadow-inner">
                     <CardBody className="flex flex-col gap-4 text-mywhite">
                         <div className="flex flex-row w-full justify-between">
                             <Typography variant='h3'>Change Password</Typography>
@@ -81,20 +84,9 @@ export default function PasswordDialog(props) {
                             </button>
                         </div>
                         <Input
-                            variant="standard"
-                            type="text"
-                            label="Email"
-                            size="lg"
-                            autoComplete="off"
-                            color="white"
-                            className="text-mywhite"
-                            onChange={e => setEmail(e.target.value)}
-                            required={true}
-                        />
-                        <Input
                             variant='standard'
                             type="password"
-                            label='Old Password'
+                            label='Current Password'
                             size="lg"
                             autoComplete="off"
                             color="white"
@@ -116,7 +108,7 @@ export default function PasswordDialog(props) {
                         <Input
                             variant='standard'
                             type='password'
-                            label='Confirm New Password'
+                            label='Confirm Password'
                             size="lg"
                             color="white"
                             className="text-mywhite"
@@ -125,7 +117,7 @@ export default function PasswordDialog(props) {
                             required={true}
                         />
                     </CardBody>
-                    <CardFooter className="pt-0">
+                    <CardFooter>
                         <Button
                             variant="gradient"
                             onClick={handlePassChange}
@@ -133,12 +125,12 @@ export default function PasswordDialog(props) {
                             color="white"
                             className="mb-4"
                         >
-                            Confirm
+                            Submit
                         </Button>
                         {
                             !checkPass && newPass &&
                             <Alert
-                                className="bg-yellow-800"
+                                className="bg-yellow-800 mb-2"
                                 icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                     <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
                                 </svg>
@@ -148,7 +140,7 @@ export default function PasswordDialog(props) {
                                     variant="paragraph"
                                     className="text-sm tracking-wide"
                                 >
-                                    New Password Fields do not match
+                                    Password and Confirm Password don&apos;t match
                                 </Typography>
                             </Alert>
                         }
@@ -167,6 +159,7 @@ export default function PasswordDialog(props) {
                                 >
                                     Password Reset successfull
                                 </Typography>
+                                <PostChange handleProfileDialog={props.handleProfileDialog} />
                             </Alert>
                         }
                         {
@@ -181,14 +174,6 @@ export default function PasswordDialog(props) {
                                 {errMsg}
                             </Alert>
                         }
-                        {/* <Typography className='mt-4 text-mywhite/80'>
-                            Already have an account? {" "}
-                            <span
-                                onClick={() => { props.handleRegisterDialog(); props.handleLoginDialog(); }} className='font-medium text-myRed3 transition-colors hover:text-myRed3/70 cursor-pointer'
-                            >
-                                Login
-                            </span>
-                        </Typography> */}
                     </CardFooter>
                 </Card>
             </Dialog>
---
+++ client/src/components/PostChange.jsx
@@ -0,0 +1,43 @@
+import React, { useRef, useState, useEffect, useContext } from "react";
+import PropTypes from 'prop-types';
+import AuthContext from "../context/AuthProvider";
+import { useNavigate } from "react-router-dom";
+import { Typography } from "@material-tailwind/react";
+
+PostChange.propTypes = {
+    handleProfileDialog: PropTypes.func,
+};
+
+export default function PostChange(props) {
+    const [num, setNum] = useState(10);
+    const { setUsername, setAuth, setAuthSuccess } = useContext(AuthContext);
+    const navigate = useNavigate();
+    let intervalRef = useRef();
+
+    const decreaseNum = () => setNum((prev) => prev - 1);
+
+    useEffect(() => {
+        intervalRef.current = setInterval(decreaseNum, 1000);
+        return () => clearInterval(intervalRef.current);
+    }, []);
+
+    if (num === 0) {
+        localStorage.clear();
+        setUsername("");
+        setAuth({});
+        setAuthSuccess(false);
+        props.handleProfileDialog();
+        navigate("/");
+    }
+
+    return (
+        <React.Fragment>
+            <Typography
+                variant="paragraph"
+                className="text-xs tracking-wide"
+            >
+                You will be redirected in {num} seconds.
+            </Typography>
+        </React.Fragment>
+    )
+}
