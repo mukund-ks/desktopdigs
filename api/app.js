a4ef707535edf905c4c468dac7f161b5f5c92522
Mukund Kumar <sankhyan.mukund@gmail.com>
2023-09-11 22:23:41 +0530

    :heavy_plus_sign: Add cors package

--- api/app.js
+++ api/app.js
@@ -1,11 +1,22 @@
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import Express from 'express';
 import mongoose from 'mongoose';
 import logger from 'morgan';
 import imageRoutes from './routes/image.js';
 import staticRoute from './routes/static.js';
 import userRoutes from './routes/user.js';
+
 const app = Express();
+
+const allowedOrigins = [
+    'http://localhost:5173',
+]
+const corsOptions = {
+    origin: allowedOrigins,
+    allowedHeaders: ['Origin, X-Requested-With, Content-Type, Accept, Authorization'],
+    credentials: true,
+}
 
 try {
     mongoose.connect(`mongodb+srv://mukundKS:${process.env.MONGODB_PASS}@ddcluster.p4gf2tv.mongodb.net/?retryWrites=true&w=majority`);
@@ -21,12 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
-    next();
-});
+app.use(cors(corsOptions));
 
 app.use('/', staticRoute);
 
